b779cc4614706f59c67a63e289f5fb5f47234e75
James <james@team-wombat.com>
2016-07-05 17:49:35 -0400

    Huge update. Built more pages.

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,3 +1,12 @@
 class Dish < ActiveRecord::Base
+    belongs_to :user
+    before_save :remove_blanks_from_arrays
+
+    private
+    def remove_blanks_from_arrays
+        self.ingredients.delete_if &:empty?
+        self.category.delete_if {|category| category == ""}
+        self.tags.delete_if {|tag| tag == ""}
+    end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable and :omniauthable
+    devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+    has_many :dishes
+    before_save :remove_blanks_from_arrays
 
+    private
+    def remove_blanks_from_arrays
+        self.ethnicities.delete_if &:empty?
+    end
+
+    # enum ethnicities: [ :Mexican, :Italian, :Chinese, :Thai, :American, :French, :Spanish, :Indian, :Japanese ]
 end
